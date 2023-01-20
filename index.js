ccf062ce9bedb3fe6209d7e7c07dca45d227b09c
arfiligol <arfiligol@gmail.com>
2023-01-20 10:58:02 +0800

    forgot to update index.js

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+// The validate module
 // const Joi = require('joi');
+
+// The route middlewares
 const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+
+// The api module
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 app.use("/api/genres", genres);
+app.use("/api/customers", customers);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
